08ca0b04150b65c0b7d26ec94c814597745c1c4e
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-05-04 12:35:23 +0800

    update 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pybdshadow",
-    version="0.3.0",
+    version="0.3.1",
     author="Qing Yu",
     author_email="qingyu0815@foxmail.com",
     description="Python package to generate building shadow geometry",
